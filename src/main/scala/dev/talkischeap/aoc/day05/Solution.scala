d6e4e3ae5dc7e347fb372ce2516e23abcbd4ca8d
Michał Perlak <michalperlak@windowslive.com>
2019-12-05 22:27:00 +0100

    Day 05

--- src/main/scala/dev/talkischeap/aoc/day05/Solution.scala
+++ src/main/scala/dev/talkischeap/aoc/day05/Solution.scala
@@ -3,31 +3,38 @@ package dev.talkischeap.aoc.day05
 import scala.io.{Source, StdIn}
 
 object Solution {
-  def partOneSolution(): Int = {
+  def main(args: Array[String]): Unit = {
+    Solution.solution()
+  }
+
+  def solution(): Unit = {
     val input = readInput()
     executeProgram(input)
-    input(0)
   }
 
   private def executeProgram(input: Array[Int]): Unit = {
     @scala.annotation.tailrec
-    def loop(currentIndex: Int, output: List[Int]): Unit = {
+    def loop(currentIndex: Int): Unit = {
       val opcode = input(currentIndex)
       if (opcode == 99) return
-      val (opPointer, newOutput) = executeOp(opcode, input, currentIndex + 1, output)
-      loop(opPointer, newOutput)
+      val opPointer = executeOp(opcode, input, currentIndex + 1)
+      loop(opPointer)
     }
 
-    loop(0, List())
+    loop(0)
   }
 
-  private def executeOp(opcode: Int, input: Array[Int], startIndex: Int, output: List[Int]): (Int, List[Int]) = {
+  private def executeOp(opcode: Int, input: Array[Int], startIndex: Int): Int = {
     val digits = opcode.toString.toCharArray
     val operation = digits.last - '0'
-    if (operation == 1) (executeAdd(input, digits, startIndex), output)
-    else if (operation == 2) (executeMultiply(input, digits, startIndex), output)
-    else if (operation == 3) (startIndex + 1, output :+ getInput)
-    else if (operation == 4) (outputValue(output.head, startIndex), output.tail)
+    if (operation == 1) executeAdd(input, digits, startIndex)
+    else if (operation == 2) executeMultiply(input, digits, startIndex)
+    else if (operation == 3) executeGetInput(input, startIndex, digits)
+    else if (operation == 4) executeOutput(input, startIndex, digits)
+    else if (operation == 5) executeJumpIfTrue(input, startIndex, digits)
+    else if (operation == 6) executeJumpIfFalse(input, startIndex, digits)
+    else if (operation == 7) executeLessThan(input, startIndex, digits)
+    else if (operation == 8) executeEquals(input, startIndex, digits)
     else throw new IllegalStateException()
   }
 
@@ -39,32 +46,72 @@ object Solution {
 
   private def executeBinaryOp(input: Array[Int], opcode: Array[Char],
                               parameterIndex: Int, op: (Int, Int) => Int): Int = {
-    val parameterModes = opcode.dropRight(2)
-    val fullParameterModes =
-      if (parameterModes.length < 3) Array.fill(3 - parameterModes.length)(0) ++ parameterModes.map(digit => digit - '0')
-      else parameterModes.map(digit => digit - '0')
-
-    val left = getParameter(input(parameterIndex), input, fullParameterModes(0))
-    val right = getParameter(input(parameterIndex + 1), input, fullParameterModes(1))
-    val position = getParameter(parameterIndex + 2, input, fullParameterModes(2))
+    val parameterModes = getParameterModes(opcode, 3)
+    val left = getParameter(parameterIndex, input, parameterModes(2))
+    val right = getParameter(parameterIndex + 1, input, parameterModes(1))
+    val position = getParameter(parameterIndex + 2, input, parameterModes(0))
     input(position) = op(left, right)
     parameterIndex + 3
   }
 
-  private def getParameter(index: Int, input: Array[Int], mode: Int): Int = {
-    if (mode == 0) input(index)
-    else index
+  private def executeGetInput(input: Array[Int], parameterIndex: Int, opcode: Array[Char]): Int = {
+    val parameterModes = getParameterModes(opcode, 1)
+    val inputValue = StdIn.readInt
+    val parameter = getParameter(parameterIndex, input, parameterModes(0))
+    input(parameter) = inputValue
+    parameterIndex + 1
   }
 
-  private def getInput: Int = StdIn.readInt
+  private def executeOutput(input: Array[Int], parameterIndex: Int, opcode: Array[Char]): Int = {
+    val parameterModes = getParameterModes(opcode, 1)
+    val parameter = getParameter(parameterIndex, input, parameterModes(0))
+    println(parameter)
+    parameterIndex + 1
+  }
 
-  private def outputValue(value: Int, startIndex: Int): Int = {
-    println(value)
-    startIndex + 1
+  private def executeJumpIfTrue(input: Array[Int], parameterIndex: Int, opcode: Array[Char]): Int =
+    executeJump(input, parameterIndex, opcode, a => a != 0)
+
+  private def executeJumpIfFalse(input: Array[Int], parameterIndex: Int, opcode: Array[Char]): Int =
+    executeJump(input, parameterIndex, opcode, a => a == 0)
+
+  private def executeJump(input: Array[Int], parameterIndex: Int, opcode: Array[Char], cond: (Int) => Boolean): Int = {
+    val parameterModes = getParameterModes(opcode, 2)
+    val first = getParameter(parameterIndex, input, parameterModes(1))
+    val second = getParameter(parameterIndex + 1, input, parameterModes(0))
+    if (cond(first)) second else parameterIndex + 2
+  }
+
+  private def executeLessThan(input: Array[Int], parameterIndex: Int, opcode: Array[Char]): Int =
+    executeCondStore(input, parameterIndex, opcode, (a,b) => a < b)
+
+  private def executeEquals(input: Array[Int], parameterIndex: Int, opcode: Array[Char]): Int =
+    executeCondStore(input, parameterIndex, opcode, (a,b) => a == b)
+
+  private def executeCondStore(input: Array[Int], parameterIndex: Int, opcode: Array[Char], cond: (Int, Int) => Boolean): Int = {
+    val parameterModes = getParameterModes(opcode, 3)
+    val first = getParameter(parameterIndex, input, parameterModes(2))
+    val second = getParameter(parameterIndex + 1, input, parameterModes(1))
+    val third = getParameter(parameterIndex + 2, input, parameterModes(0))
+    input(third) = if (cond(first, second)) 1 else 0
+    parameterIndex + 3
+  }
+
+  private def getParameterModes(opcode: Array[Char], parameters: Int): Array[Int] = {
+    val parameterModes = opcode.dropRight(2)
+    if (parameterModes.length < parameters)
+      Array.fill(parameters - parameterModes.length)(0) ++ parameterModes.map(digit => digit - '0')
+    else
+      parameterModes.map(digit => digit - '0')
+  }
+
+  private def getParameter(index: Int, input: Array[Int], mode: Int): Int = {
+    if (mode == 0) input(input(index))
+    else input(index)
   }
 
   private def readInput(): Array[Int] = Source
-    .fromResource("day05/input.txt")
+    .fromResource("day05/test.txt")
     .getLines
     .flatMap(parseLine)
     .toArray
